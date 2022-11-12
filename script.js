7b9d29b6ffffd89fce15eeeb6eb841d0e21942bd
Emmachiabuotu <heemenkonial@gmail.com>
2022-11-12 15:00:13 +0100

    my first commit

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const buttons = document.querySelectorAll("[data-carousel-button]")
+let slideIndex = 1;
+showSlides(slideIndex);
 
-buttons.forEach(button => {
-    button.addEventListener("click",() =>{
-        const offset = button.dataset.carouselButton === "next" ? 1 : -1
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+// Controls for Next and Previous
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-        const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...,slides.children].indexOf(activeSlide) + offset
-        if (newIndex < 0) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide.dataset.active
-    })
-})
